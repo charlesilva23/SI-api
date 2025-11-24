02d9a6c1955668932b5bda9cf42572c37333f3af
Charles Silva <charlesilva2323@gmail.com>
2025-11-24 17:49:44 -0300

    feat: update ticket

--- src/main/java/com/inova/siapi/modules/ticket/controller/TicketController.java
+++ src/main/java/com/inova/siapi/modules/ticket/controller/TicketController.java
@@ -4,8 +4,11 @@ import com.inova.siapi.modules.ticket.dtos.TicketCreateDTO;
 import com.inova.siapi.modules.ticket.dtos.TicketResponseDTO;
 import com.inova.siapi.modules.ticket.entities.Ticket;
 import com.inova.siapi.modules.ticket.entities.enums.TicketStatusEnum;
+import com.inova.siapi.modules.ticket.repositories.TicketRepository;
 import com.inova.siapi.modules.ticket.services.TicketService;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +22,11 @@ import java.util.List;
 public class TicketController {
 
     private final TicketService ticketService;
+    private final TicketRepository ticketRepository;
 
-    public TicketController(TicketService ticketService) {
+    public TicketController(TicketService ticketService, TicketRepository ticketRepository) {
         this.ticketService = ticketService;
+        this.ticketRepository = ticketRepository;
     }
 
 
@@ -33,8 +38,19 @@ public class TicketController {
 
     @GetMapping
     public ResponseEntity<List<TicketResponseDTO>> listTickets() {
-        List<TicketResponseDTO> tickets = ticketService.listAll();
-        return ResponseEntity.status(200).body(tickets);
+        List<Ticket> tickets = ticketRepository.findAll(Sort.by("id"));
+        return ResponseEntity.ok(
+                tickets.stream().map(TicketResponseDTO::new).toList()
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TicketResponseDTO> updateTicket(
+            @PathVariable Integer id,
+            @Valid @RequestBody TicketCreateDTO dto
+    ) {
+        TicketResponseDTO updated = ticketService.update(id, dto);
+        return ResponseEntity.status(201).body(updated);
     }
 
 }
